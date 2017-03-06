be42dd12353303ec78d2eb07779dd0a9caccf069
Manne Öhrström <manne@shotgunsoftware.com>
2017-03-06 10:08:30 +0000

    updated max hooks to match latest publisher interface changes.

--- hooks/3dsmax/publish/collector.py
+++ hooks/3dsmax/publish/collector.py
@@ -15,60 +15,56 @@ from pymxs import runtime as mxs_rt
 
 HookBaseClass = sgtk.get_hook_baseclass()
 
-class GenericSceneCollector(HookBaseClass):
+class MaxSceneCollector(HookBaseClass):
     """
-    Collector that operates on the maya scene
+    Collector that operates on the max scene
     """
 
     def process_current_scene(self, parent_item):
+        """
+        Analyzes the current scene open in a DCC and parents a subtree of items
+        under the parent_item passed in.
+
+        :param parent_item: Root item instance
+        """
         item = self.create_current_3dsmax_scene(parent_item)
         return item
 
     def process_file(self, parent_item, path):
+        """
+        Analyzes the given file and creates one or more items
+        to represent it. Extends the base processing
+        capabilities with a maya file detection which
+        determines the maya project.
 
-        file_name = os.path.basename(path)
-        (file_name_no_ext, file_extension) = os.path.splitext(file_name)
+        :param parent_item: Root item instance
+        :param path: Path to analyze
+        :returns: The main item that was created
+        """
 
-        if file_extension in [".jpeg", ".jpg", ".png"]:
-            file_item = parent_item.create_item("file.image", "Image File", file_name)
-            file_item.set_thumbnail(path)
-            file_item.set_icon(os.path.join(self.disk_location, "icons", "image.png"))
+        if path.endswith(".max"):
 
-        elif file_extension in [".mov", ".mp4"]:
-            file_item = parent_item.create_item("file.movie", "Movie File", file_name)
-            file_item.set_icon(os.path.join(self.disk_location, "icons", "quicktime.png"))
+            # run base class logic to set basic properties for us
+            item = super(MaxSceneCollector, self).process_file(parent_item, path)
 
-        elif file_extension in [".max"]:
-            file_item = parent_item.create_item("file.3dsmax", "3dsMax File", file_name)
-            file_item.set_icon(os.path.join(self.disk_location, "icons", "3dsmax.png"))
+            item.type = "file.3dsmax"
+            item.display_type = "3dsMax File"
+            item.set_icon_from_path(os.path.join(self.disk_location, "icons", "3dsmax.png"))
 
             folder = os.path.dirname(path)
             if os.path.basename(folder) == "scenes":
                 # assume parent level is workspace root
-                file_item.properties["project_root"] = os.path.dirname(folder)
+                item.properties["project_root"] = os.path.dirname(folder)
             else:
-                file_item.properties["project_root"] = None
+                item.properties["project_root"] = None
 
         else:
-            file_item = parent_item.create_item("file", "Generic File", file_name)
-            file_item.set_icon(os.path.join(self.disk_location, "icons", "page.png"))
 
-        file_item.properties["extension"] = file_extension
-        file_item.properties["path"] = path
-        file_item.properties["filename"] = file_name
+            # run base class implementation
+            item = super(MaxSceneCollector, self).process_file(parent_item, path)
 
-        # check if path matches pattern fooo.v123.ext
-        version = re.search("(.*)\.v([0-9]+)\.[^\.]+$", file_name)
-        if version:
-            # strip all leading zeroes
-            file_item.properties["prefix"] = version.group(1)
-            version_no_leading_zeroes = version.group(2).lstrip("0")
-            file_item.properties["version"] = int(version_no_leading_zeroes)
-        else:
-            file_item.properties["version"] = 0
-            file_item.properties["prefix"] = file_name_no_ext
+        return item
 
-        return file_item
 
     def create_current_3dsmax_scene(self, parent_item):
         # get the main scene:
@@ -82,6 +78,6 @@ class GenericSceneCollector(HookBaseClass):
         current_scene = parent_item.create_item("3dsmax.scene", "Current 3dsMax Scene", scene_file_name)
         current_scene.properties["path"] = scene_file_name_and_path
         current_scene.properties["project_root"] = mxs_rt.pathConfig.getCurrentProjectFolder()
-        current_scene.set_icon(os.path.join(self.disk_location, "icons", "3dsmax.png"))
+        current_scene.set_icon_from_path(os.path.join(self.disk_location, "icons", "3dsmax.png"))
         return current_scene
 
