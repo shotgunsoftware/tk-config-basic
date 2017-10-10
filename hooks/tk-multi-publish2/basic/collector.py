58a3152839e3a985fb27b38af3ed6d7c091cc573
fhawi <firas.hawi@autodesk.com>
2017-10-10 11:07:54 -0400

    Encoded type and display names in utf-8

--- hooks/tk-multi-publish2/basic/collector.py
+++ hooks/tk-multi-publish2/basic/collector.py
@@ -119,8 +119,8 @@ class BasicSceneCollector(HookBaseClass):
 
         # get info for the extension
         item_info = self._get_item_info(path)
-        item_type = item_info["item_type"]
-        type_display = item_info["type_display"]
+        item_type = item_info["item_type"].encode("utf-8")
+        type_display = item_info["type_display"].encode("utf-8")
         evaluated_path = path
         is_sequence = False
 
